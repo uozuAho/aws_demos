53684e3641e265d837a90b54fe9f24b13e75af59
Warwick Stone <warwick_stone@fastmail.com>
2022-04-24 11:02:22 +1000

    serverless: add widget service

--- serverless_example/MyWidgetService/lib/my_widget_service-stack.ts
+++ serverless_example/MyWidgetService/lib/my_widget_service-stack.ts
@@ -1,16 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as widget_service from './widget_service';
 
 export class MyWidgetServiceStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyWidgetServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new widget_service.WidgetService(this, 'Widgets');
   }
 }
