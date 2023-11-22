007c186b08b0f889ef1f8a665674cfb702bd1f5a
Warwick Stone <warwick_stone@fastmail.com>
2023-11-22 14:32:32 +1100

    add api gateway

--- cdk_demo_app/cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk_demo_app/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,6 +11,10 @@ export class CdkWorkshopStack extends Stack {
       runtime: lambda.Runtime.NODEJS_20_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler',
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     });
   }
 }
