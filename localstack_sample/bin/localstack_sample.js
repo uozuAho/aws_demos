7d138f7ed9fc59baaceedbab3738d43afe2e8578
Warwick Stone <warwick_stone@fastmail.com>
2023-11-23 12:42:16 +1100

    remove localstack and serverless examples

--- localstack_sample/bin/localstack_sample.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-const cdk = require('aws-cdk-lib');
-const { LocalstackSampleStack } = require('../lib/localstack_sample-stack');
-
-const app = new cdk.App();
-new LocalstackSampleStack(app, 'LocalstackSampleStack');
--- localstack_sample/start-localstack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it -p 4566:4566 -p 4571:4571 localstack/localstack \
-  -e "SERVICES=serverless,sqs,sns"
