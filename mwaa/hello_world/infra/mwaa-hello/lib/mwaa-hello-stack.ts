df626944ca34aefd528dce746ce8565282f939ad
Warwick Stone <warwick_stone@fastmail.com>
2024-01-01 17:11:49 +1100

    add bucket, vpc, mwaa env

--- mwaa/hello_world/infra/mwaa-hello/lib/mwaa-hello-stack.ts
+++ mwaa/hello_world/infra/mwaa-hello/lib/mwaa-hello-stack.ts
@@ -1,8 +1,58 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class MwaaHelloStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const bucket = new cdk.aws_s3.Bucket(this, 'Bucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: true,
+    });
+
+    const vpc = new cdk.aws_ec2.Vpc(this, 'Vpc', {
+      availabilityZones: ['ap-southeast-2a', 'ap-southeast-2b'],
+      ipAddresses: ec2.IpAddresses.cidr('10.192.0.0/16'),
+    });
+
+    const securityGroup = new cdk.aws_ec2.SecurityGroup(this, 'MwaaSecurityGroup', {
+      vpc,
+    });
+    securityGroup.addIngressRule(securityGroup, ec2.Port.allTraffic(),
+      "allow all traffic from within the security group");
+
+    const mwaaEnvironment = new cdk.aws_mwaa.CfnEnvironment(this, 'MwaaEnvironment', {
+      name: 'mwaa-hello',
+      // airflowVersion: '2.0.2', // latest if omitted
+      dagS3Path: `dags`,
+      environmentClass: 'mw1.small',
+      // executionRoleArn: 'arn:aws:iam::123456789012:role/airflow-role',
+      // loggingConfiguration: {
+      //   taskLogs: {
+      //     cloudWatchLogGroupArn: 'arn:aws:logs:ap-southeast-2:123456789012:log-group:airflow-task-logs',
+      //     enabled: true,
+      //   },
+      //   webserverLogs: {
+      //     cloudWatchLogGroupArn: 'arn:aws:logs:ap-southeast-2:123456789012:log-group:airflow-webserver-logs',
+      //     enabled: true,
+      //   },
+      //   workerLogs: {
+      //     cloudWatchLogGroupArn: 'arn:aws:logs:ap-southeast-2:123456789012:log-group:airflow-worker-logs',
+      //     enabled: true,
+      //   },
+      // },
+      maxWorkers: 2,
+      minWorkers: 0,
+      networkConfiguration: {
+        securityGroupIds: [securityGroup.securityGroupId],
+        subnetIds: vpc.publicSubnets.map((subnet) => subnet.subnetId),
+      },
+      requirementsS3Path: `requirements.txt`,
+      sourceBucketArn: bucket.bucketArn,
+      webserverAccessMode: 'PUBLIC_ONLY',
+      weeklyMaintenanceWindowStart: 'SUN:08:00',
+    });
   }
 }
---
+++ mwaa/hello_world/readme.md
@@ -0,0 +1,4 @@
+# AWS MWAA hello world
+
+Built following https://docs.aws.amazon.com/mwaa/latest/userguide/vpc-create.html
+Also used https://github.com/aws-samples/cdk-amazon-mwaa-cicd
