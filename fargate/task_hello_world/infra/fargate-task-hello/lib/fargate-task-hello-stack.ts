8323d011c8450565741125bbc13bf1966335266b
Warwick Stone <warwick_stone@fastmail.com>
2024-01-02 13:03:58 +1100

    create vpc and cluster

--- fargate/task_hello_world/infra/fargate-task-hello/lib/fargate-task-hello-stack.ts
+++ fargate/task_hello_world/infra/fargate-task-hello/lib/fargate-task-hello-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 
 export class FargateTaskHelloStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      availabilityZones: ['ap-southeast-2a', 'ap-southeast-2b'],
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FargateTaskHelloQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const ecsCluster = new ecs.Cluster(this, 'EcsCluster', {
+      vpc: vpc,
+    });
   }
 }
